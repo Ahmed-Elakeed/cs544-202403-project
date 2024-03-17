acbec65e0727ddea4a58bd32e81b1d3d81e492e2
ahmedelakeed <ahmed.elaked20@gmail.com>
2024-03-17 03:57:11 -0500

    scan packages warn fix

--- src/main/java/edu/miu/cs/cs544/Application.java
+++ src/main/java/edu/miu/cs/cs544/Application.java
@@ -7,19 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import edu.miu.cs.cs544.service.MemberService;
 
-@SpringBootApplication(scanBasePackages = "edu.miu.common, edu.miu.cs.cs544")
+@SpringBootApplication(scanBasePackages = {"edu.miu.common", "edu.miu.cs.cs544"})
 public class Application implements CommandLineRunner {
 
-	@Autowired
-	private MemberService service;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Autowired
+    private MemberService service;
 
-	@Override
-	public void run(String... args) throws Exception {
-		service.findAll().forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        service.findAll().forEach(System.out::println);
+    }
 
 }
