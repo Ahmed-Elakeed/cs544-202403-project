be9beae8feac088e0ec5cf6b21b4ea6f0361f353
ahmedelakeed <ahmed.elaked20@gmail.com>
2024-03-18 10:47:35 -0500

    sessions crud

---
+++ src/main/java/edu/miu/cs/cs544/service/EventService.java
@@ -0,0 +1,21 @@
+package edu.miu.cs.cs544.service;
+
+import edu.miu.common.service.BaseReadWriteService;
+import edu.miu.cs.cs544.domain.Event;
+import edu.miu.cs.cs544.service.contract.EventPayload;
+import edu.miu.cs.cs544.service.contract.SessionPayload;
+
+import java.util.List;
+
+public interface EventService extends BaseReadWriteService<EventPayload, Event, Long> {
+
+     List<SessionPayload> getAllSessionsForEvent(Long eventId);
+
+     SessionPayload getSessionForEvent(Long eventId, Long sessionId);
+
+     SessionPayload saveSessionForEvent(Long eventId, SessionPayload sessionPayload);
+
+     SessionPayload updateSessionInEvent(Long eventId, Long sessionId, SessionPayload sessionPayload);
+
+     String deleteSessionFromEvent(Long eventId, Long sessionId);
+}
---
+++ src/main/java/edu/miu/cs/cs544/service/EventServiceImpl.java
@@ -0,0 +1,102 @@
+package edu.miu.cs.cs544.service;
+
+import edu.miu.common.service.BaseReadWriteServiceImpl;
+import edu.miu.cs.cs544.domain.Event;
+import edu.miu.cs.cs544.domain.Session;
+import edu.miu.cs.cs544.exception.NotFoundException;
+import edu.miu.cs.cs544.repository.EventRepository;
+import edu.miu.cs.cs544.service.contract.EventPayload;
+import edu.miu.cs.cs544.service.contract.SessionPayload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl extends BaseReadWriteServiceImpl<EventPayload, Event, Long> implements EventService {
+
+    private final EventRepository eventRepository;
+
+    @Override
+    public List<SessionPayload> getAllSessionsForEvent(Long eventId) {
+        return this.eventRepository.fetchAllSessionForEvent(eventId);
+    }
+
+    @Override
+    public SessionPayload getSessionForEvent(Long eventId, Long sessionId) {
+        Optional<Event> eventOptional = this.eventRepository.findById(eventId);
+        if (eventOptional.isPresent()) {
+            Optional<Session> sessionOptional = eventOptional.get()
+                    .getSessions()
+                    .stream()
+                    .filter(session -> session.getId().equals(sessionId))
+                    .findFirst();
+            if (sessionOptional.isPresent()) {
+                Session session = sessionOptional.get();
+                return new SessionPayload(
+                        session.getId(),
+                        session.getName(),
+                        session.getDescription(),
+                        session.getStartDateTime(),
+                        session.getEndDateTime()
+                );
+            }
+        }
+        throw new NotFoundException("Event or session not exist");
+    }
+
+    @Override
+    public SessionPayload saveSessionForEvent(Long eventId, SessionPayload sessionPayload) {
+        Optional<Event> eventOptional = this.eventRepository.findById(eventId);
+        if (eventOptional.isPresent()) {
+            Event event = eventOptional.get();
+            Session session = new Session();
+            session.setName(sessionPayload.getName());
+            session.setDescription(sessionPayload.getDescription());
+            session.setStartDateTime(sessionPayload.getStartDateTime());
+            session.setEndDateTime(sessionPayload.getEndDateTime());
+            session.setEvent(event);
+            event.getSessions().add(session);
+            this.eventRepository.save(event);
+            return sessionPayload;
+        }
+        throw new NotFoundException("Event not exist");
+    }
+
+    @Override
+    public SessionPayload updateSessionInEvent(Long eventId, Long sessionId, SessionPayload sessionPayload) {
+        Optional<Event> eventOptional = this.eventRepository.findById(eventId);
+        if (eventOptional.isPresent()) {
+            Event event = eventOptional.get();
+            Optional<Session> sessionOptional = event
+                    .getSessions()
+                    .stream()
+                    .filter(session -> session.getId().equals(sessionId))
+                    .findFirst();
+            if(sessionOptional.isPresent()){
+                Session session = sessionOptional.get();
+                session.setName(sessionPayload.getName());
+                session.setDescription(sessionPayload.getDescription());
+                session.setStartDateTime(sessionPayload.getStartDateTime());
+                session.setEndDateTime(sessionPayload.getEndDateTime());
+                this.eventRepository.save(event);
+                return sessionPayload;
+            }
+        }
+        throw new NotFoundException("Event or session not exist");
+    }
+
+    @Override
+    public String deleteSessionFromEvent(Long eventId, Long sessionId) {
+        Optional<Event> eventOptional = this.eventRepository.findById(eventId);
+        if (eventOptional.isPresent()) {
+            this.eventRepository.deleteSessionFromEvent(eventId,sessionId);
+            return "Session deleted or it was already not exist for this event";
+        }
+        throw new NotFoundException("This event not exist");
+    }
+}
