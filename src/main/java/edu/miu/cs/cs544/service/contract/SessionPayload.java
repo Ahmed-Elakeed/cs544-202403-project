9b03b0dbe7ad7f9d3d38c2e21b5793e427d74409
ahmedelakeed <ahmed.elaked20@gmail.com>
2024-03-18 18:37:02 -0500

    session mapper

--- src/main/java/edu/miu/cs/cs544/service/contract/SessionPayload.java
+++ src/main/java/edu/miu/cs/cs544/service/contract/SessionPayload.java
@@ -1,6 +1,7 @@
 package edu.miu.cs.cs544.service.contract;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serial;
@@ -9,6 +10,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class SessionPayload implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
