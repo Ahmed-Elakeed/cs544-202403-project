dbc48f17f3992ab305a6a1c00d64b85149384d88
ahmedelakeed <ahmed.elaked20@gmail.com>
2024-03-18 13:59:21 -0500

    domain changes

--- src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
+++ src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
@@ -2,10 +2,6 @@ package edu.miu.cs.cs544.service.contract;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
-
-import edu.miu.cs.cs544.domain.Account;
-import edu.miu.cs.cs544.domain.Role;
 import lombok.Data;
 
 @Data
@@ -19,6 +15,4 @@ public class MemberPayload implements Serializable {
 	private String lastName;
 	private String email;
 	private int barcode;
-	private List<Role> roles;
-	private List<Account> accounts;
 }
