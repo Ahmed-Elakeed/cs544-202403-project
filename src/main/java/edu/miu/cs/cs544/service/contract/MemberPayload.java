c2358a7b64390668aaba14834231e2ea0d193ea4
pmajoka <pmajoka@gmail.com>
2024-03-17 19:37:08 -0500

    member crud operation done

--- src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
+++ src/main/java/edu/miu/cs/cs544/service/contract/MemberPayload.java
@@ -2,7 +2,10 @@ package edu.miu.cs.cs544.service.contract;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.List;
 
+import edu.miu.cs.cs544.domain.Account;
+import edu.miu.cs.cs544.domain.Role;
 import lombok.Data;
 
 @Data
@@ -12,13 +15,10 @@ public class MemberPayload implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	
 	private String firstName;
-
 	private String lastName;
-
 	private String email;
-
 	private int barcode;
-
+	private List<Role> roles;
+	private List<Account> accounts;
 }
