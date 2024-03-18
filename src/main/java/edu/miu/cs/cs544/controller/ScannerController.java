7aee18d1bd9058c711be10e0c8e14dcc9a9134d1
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-03-18 16:40:57 -0500

    scanner cleaned

--- src/main/java/edu/miu/cs/cs544/controller/ScannerController.java
+++ src/main/java/edu/miu/cs/cs544/controller/ScannerController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/scanners")
-public class BadgeScannerController
+public class ScannerController
         extends BaseReadWriteController<ScannerPayload, Scanner, Long> {
 }
