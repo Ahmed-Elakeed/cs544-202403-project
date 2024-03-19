ad57dc1867d03677650b13b262d5d1d915aededf
vohoangxuan <hoangxuanvot@gmail.com>
2024-03-19 14:00:04 -0500

    conflicts solving

--- src/main/java/edu/miu/cs/cs544/controller/MemberController.java
+++ src/main/java/edu/miu/cs/cs544/controller/MemberController.java
@@ -1,17 +1,15 @@
 package edu.miu.cs.cs544.controller;
 
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import edu.miu.common.controller.BaseReadWriteController;
 import edu.miu.cs.cs544.domain.Member;
+import edu.miu.cs.cs544.domain.Role;
 import edu.miu.cs.cs544.service.MemberService;
 import edu.miu.cs.cs544.service.contract.MemberPayload;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 
 
 @RestController
@@ -19,9 +17,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberController extends BaseReadWriteController<MemberPayload, Member, Long> {
 
-	private final MemberService memberService;
-	
-	
+    private final MemberService memberService;
+    @GetMapping(path = "/{memberId}/roles")
+    public ResponseEntity<?> getAllRoles(@PathVariable(value = "memberId") Long memberId) {
+        return ResponseEntity.ok(this.memberService.getAllRolesForMember(memberId));
+    }
+
+    @GetMapping(path = "/{memberId}/roles/{roleId}")
+    public ResponseEntity<?> getRoleForMember(@PathVariable(value = "memberId") Long memberId,@PathVariable(value = "roleId") Long roleId) {
+        return ResponseEntity.ok(this.memberService.getRoleForMember(memberId,roleId));
+    }
+
+    @PostMapping(path = "/{memberId}/roles")
+    public ResponseEntity<?> createRole(@PathVariable(value = "memberId") Long memberId,@RequestBody Role role) {
+        return ResponseEntity.ok(this.memberService.createRole(memberId,role));
+    }
+
+    @PutMapping("/{memberId}/roles")
+    public ResponseEntity<?> updateRole(@PathVariable(value = "memberId") Long memberId, @RequestBody Role role) {
+        return ResponseEntity.ok(this.memberService.updateRole(memberId, role));
+    }
+
+    @DeleteMapping("/{memberId}/roles/{roleId}")
+    public ResponseEntity<?> deleteRole(@PathVariable(value = "memberId") Long memberId, @PathVariable(value = "roleId") Long roleId) {
+        return ResponseEntity.ok(this.memberService.deleteRoleForMember(memberId,roleId));
+    }
+
+    @GetMapping(path = "/{memberId}/attendance")
+    public ResponseEntity<?> getAllSessionsForEvent(@PathVariable(value = "memberId") Long memberId) {
+        return ResponseEntity.ok(this.memberService.getAttendence(memberId));
+    }
+    
 	@GetMapping(path = "/{memberId}/events/{eventId}/attendance")
     public ResponseEntity<?> attendanceForMemberByEvent(@PathVariable(value = "memberId") Long memberId,@PathVariable(value = "eventId") Long eventId){
         return ResponseEntity.ok(this.memberService.attendanceForMemberByEvent(memberId,eventId));
--- src/main/java/edu/miu/cs/cs544/service/MemberService.java
+++ src/main/java/edu/miu/cs/cs544/service/MemberService.java
@@ -1,11 +1,28 @@
 package edu.miu.cs.cs544.service;
 
+import java.util.List;
+
 import edu.miu.common.service.BaseReadWriteService;
 import edu.miu.cs.cs544.domain.Member;
+import edu.miu.cs.cs544.domain.Role;
 import edu.miu.cs.cs544.dto.AttendanceResponseDTO;
 import edu.miu.cs.cs544.service.contract.MemberPayload;
 
+
+
 public interface MemberService extends BaseReadWriteService <MemberPayload, Member, Long>{
 
+    List<Role> getAllRolesForMember(Long memberId);
+
+    AttendanceResponseDTO getAttendence(Long id);
+
+    Role getRoleForMember(Long memberId, Long roleId);
+
+    Role createRole(Long memberId, Role role);
+
+    Role updateRole(Long memberId, Role role);
+
+    String deleteRoleForMember(Long memberId, Long roleId);
+    
 	AttendanceResponseDTO attendanceForMemberByEvent(Long memberId, Long eventId);
 }
