89b704344d4deff0fba5ff9c521ffe2de9dd0fef
vijay9711 <vijaysm97@gmail.com>
2024-03-20 11:18:20 -0500

    event Test controller

--- src/test/java/edu/miu/cs/cs544/Controller/EventControllerTest.java
+++ src/test/java/edu/miu/cs/cs544/Controller/EventControllerTest.java
@@ -1,73 +1,67 @@
 package edu.miu.cs.cs544.Controller;
 
-import com.nimbusds.jose.shaded.gson.Gson;
-import com.nimbusds.jose.shaded.gson.JsonObject;
+import edu.miu.cs.cs544.controller.AccountController;
+import edu.miu.cs.cs544.controller.EventController;
 import edu.miu.cs.cs544.dto.AttendanceResponseDTO;
 import edu.miu.cs.cs544.mapper.JsonParser;
 import edu.miu.cs.cs544.repository.EventRepository;
 import edu.miu.cs.cs544.service.EventService;
-import edu.miu.cs.cs544.service.contract.AttendanceDTO;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssumptions.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(EventController.class)
+@AutoConfigureMockMvc(addFilters = false)
+//@SpringBootTest(classes = Appendable.class)
 public class EventControllerTest {
-
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     EventService eventService;
-
-    @Mock
+    @MockBean
     EventRepository eventRepository;
 
-
+    private AttendanceResponseDTO expectedData;
     @Test
-    public void getAttendanceByEventIdTest() throws Exception {
+    public void getAttendanceForEventTest() throws Exception {
+        String jsonArray = "{\"count\":3,\"attendanceRecordList\":[{\"memberId\":1,\"memberFirstName\":\"voijay\",\"memberLastName\":\"mano\",\"sessionId\":1,\"sessionName\":\"Session 1\",\"sessionDescription\":\"Monday\"}]}";
+        expectedData = JsonParser.parseJsonToObject(jsonArray, AttendanceResponseDTO.class);
+        Mockito.when(eventService.getAttendanceForEvent(1L)).thenReturn(expectedData);
 
-//        String expectedData = "{\"count\":3,\"attendanceRecordList\":[{\"memberId\":1,\"memberFirstName\":\"voijay\",\"memberLastName\":\"mano\",\"sessionId\":1,\"sessionName\":\"Session 1\",\"sessionDescription\":\"Monday\"},{\"memberId\":2,\"memberFirstName\":\"test\",\"memberLastName\":\"man\",\"sessionId\":1,\"sessionName\":\"Session 1\",\"sessionDescription\":\"Monday\"},{\"memberId\":3,\"memberFirstName\":\"rim\",\"memberLastName\":\"cok\",\"sessionId\":1,\"sessionName\":\"Session 1\",\"sessionDescription\":\"Monday\"}]}";
-////
-//        AttendanceResponseDTO jsonObject = JsonParser.parseJsonToObject(expectedData, AttendanceResponseDTO.class);
-////        ResponseEntity<?> response = ResponseEntity.ok(eventService.getAttendanceForEvent(1L));
-////        System.out.print(jsonObject+  " ------------------------> " + response.getBody());
-//        Mockito.when(eventService.getAttendanceForEvent(1L)).thenReturn(jsonObject);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/events/{eventId}/attendance", 1))
-//                .andExpect(status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.count").value("3"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/events/{eventId}/attendance", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.count").value(3)) // Verify the count
+                .andExpect(MockMvcResultMatchers.jsonPath("$.attendanceRecordList[0].memberId").value(1));
     }
 
     @Test
-    public void getAllSessionForEventTest(){
-        //given
-        Long eventId = 1L;
-
-        //response
-        ResponseEntity<?> response = ResponseEntity.ok(this.eventService.getAttendanceForEvent(eventId));
-
-        //expected data
-
+    public void getAllSessionsForEventTest(){
+        String json = "{\"id\":1,\"name\":\"Session 1\",\"description\":\"Monday\",\"startDateTime\":\"2024-03-18T22:30:23.000+00:00\",\"endDateTime\":\"2024-03-19T01:30:00.000+00:00\"}";
 
     }
+
 }
--- src/test/java/edu/miu/cs/cs544/Service/EventServiceTest.java
+++ src/test/java/edu/miu/cs/cs544/Service/EventServiceTest.java
@@ -17,23 +17,19 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Appendable.class)
 @AutoConfigureMockMvc
 public class EventServiceTest {
 
-    @Mock
+    @Autowired
     EventService eventService;
 
 //    @Mock
@@ -52,6 +48,37 @@ public class EventServiceTest {
     @Test
     public void getAllSessionsForEventTest() throws Exception {
         assertThat(sessionList).isEqualTo(eventService.getAllSessionsForEvent(1L));
+    }
+
+    @Test
+    public void getSessionForEventTest() throws Exception {
+        String data = "{\"id\":1,\"name\":\"Session 1\",\"description\":\"Monday\",\"startDateTime\":\"2024-03-18T22:30:23.000+00:00\",\"endDateTime\":\"2024-03-19T01:30:00.000+00:00\"}";
+        SessionPayload expectedData = JsonParser.parseJsonToObject(data, SessionPayload.class);
+        Mockito.when(eventService.getSessionForEvent(1L, 1L)).thenReturn(expectedData);
+        assertThat(expectedData).isEqualTo(eventService.getSessionForEvent(1L, 1L));
+    }
+
+    @Test
+    public void saveSessionForEvent() throws Exception {
+        Date startDate = new Date();
+        Date endDate = new Date();
+        SessionPayload sessionData = new SessionPayload(4L,"test session", "test session description",startDate, endDate);
+        Mockito.when(eventService.saveSessionForEvent(1L, sessionData)).thenReturn(new SessionPayload(4L,"test session", "test session description",startDate, endDate));
+        assertThat(sessionData).isEqualTo(eventService.saveSessionForEvent(1L, sessionData));
+    }
+
+    @Test
+    public void updateSessionForEventTest(){
+        Date startDate = new Date();
+        Date endDate = new Date();
+        SessionPayload sessionData = new SessionPayload(4L,"test session", "test session description",startDate, endDate);
+        Mockito.when(eventService.updateSessionInEvent(4L, 1L, sessionData)).thenReturn(sessionData);
+        assertThat(sessionData).isEqualTo(eventService.updateSessionInEvent(4L,1L, sessionData));
+    }
+
+    @Test
+    public void deleteSessionFromEventTest(){
+
     }
 
 }
