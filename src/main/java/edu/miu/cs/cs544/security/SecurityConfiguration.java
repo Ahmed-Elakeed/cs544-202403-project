b2d4362ed632922247601a592667ac8dd366e819
ahmedelakeed <ahmed.elaked20@gmail.com>
2024-03-21 10:25:26 -0500

    oAuth

--- src/main/java/edu/miu/cs/cs544/security/SecurityConfiguration.java
+++ src/main/java/edu/miu/cs/cs544/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package edu.miu.cs.cs544.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,9 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/badge-system").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
+
 
 }
 
